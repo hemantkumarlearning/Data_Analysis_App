79da6982eb3fd109fd52deeb2feaa4d585d1101a
Hemant Kumar <hemantkumar08.dhn@gmail.com>
2025-03-22 17:05:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,110 +1,78 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import plotly.express as px
+import seaborn as sns
 import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
-import numpy as np
 
-def load_data(file):
-    df = pd.read_csv(file)
+
+def load_csv():
+    file = st.file_uploader("Upload your CSV", type=["csv"])
+    if file is not None:
+        df = pd.read_csv(file)
+        st.write(f"### Dataframe Shape: {df.shape}")
+        st.write(f"### Duplicates Count: {df.duplicated().sum()}")
+        st.dataframe(df.head())
+        return df
+    return None
+
+def preprocess_data(df):
+    
+    st.sidebar.header("Data Preprocessing")
+    columns_to_delete = st.sidebar.multiselect("Select columns to delete", df.columns.tolist())   
+    if columns_to_delete:
+        df = df.drop(columns=columns_to_delete)
+    
+    remove_dups = st.sidebar.checkbox("Remove Duplicates", value=True)
+    if remove_dups:
+        df = df.drop_duplicates()
+
+    
+    st.write("Data after Preprocessing:")
+    st.dataframe(df.head())
     return df
 
-def data_checks(df):
-    null_values = df.isnull().sum()
-    duplicate_values = df.duplicated().sum()
-    return null_values, duplicate_values
+def visualize_data(df):
+    st.sidebar.header("Visualizations")
+    visualize_option = st.sidebar.selectbox("Choose Visualization", ["Feature Distributions", "Target vs Features"])
 
-def visualize_numerical(df):
-    numerical_cols = df.select_dtypes(include=[np.number]).columns.tolist()
-    st.subheader("Numerical Columns Visualization:")
-    for col in numerical_cols:
-        st.subheader(f"{col} Distribution")
-        fig = px.histogram(df, x=col, title=f"Distribution of {col}",color_discrete_sequence=["#008000"])
+    if visualize_option == "Feature Distributions":
+        col = st.sidebar.selectbox("Select Feature for Distribution", df.columns)
+        fig = px.histogram(df, x=col)
         st.plotly_chart(fig)
-
-def visualize_categorical(df):
-    categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
-    st.subheader("Categorical Columns Visualization:")
-    for col in categorical_cols:
-        st.subheader(f"{col} Distribution")
-        value_counts = df[col].value_counts().reset_index()
-        value_counts.columns = [col, 'count'] 
-        fig = px.bar(value_counts, x=col, y='count', 
-                     labels={col: col, 'count': 'Count'}, 
-                     title=f"Distribution of {col}",color_discrete_sequence=["#1f77b4"])
-        st.plotly_chart(fig)
-
-def compare_with_target(df, target_column):
-    numerical_cols = df.select_dtypes(include=[np.number]).columns.tolist()
-    categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
     
-    
-    if target_column in numerical_cols:
-        numerical_cols.remove(target_column)
-    if target_column in categorical_cols:
-        categorical_cols.remove(target_column)
-
-    if len(numerical_cols) > 0:
-        st.write(f"Comparing Numerical Columns with Target: {target_column}")
-        for col in numerical_cols:
-            fig = px.scatter(df, x=col, y=target_column, 
-                             title=f"Comparison of {col} with {target_column}",color_discrete_sequence=["#FF1493"])
-            st.plotly_chart(fig)
-
-        if df[target_column].dtype in ['int64', 'float64']:  # Check if target column is numerical
-            st.subheader(f"Correlation Heatmap between Numerical Columns and {target_column}")
-            correlation_matrix = df[numerical_cols + [target_column]].corr()
-            fig, ax = plt.subplots(figsize=(10, 8))
-            sns.heatmap(correlation_matrix, annot=True, cmap="coolwarm", fmt=".2f", ax=ax)
-            st.pyplot(fig)
-
-    if len(categorical_cols) > 0:
-        st.write(f"Comparing Categorical Columns with Target: {target_column}")
-        for col in categorical_cols:
-            if df[target_column].dtype == 'object':  
-                fig = px.histogram(df, x=col, color=target_column, 
-                                    title=f"Comparison of {col} with {target_column}")
+    elif visualize_option == "Target vs Features":
+        target_column = st.sidebar.selectbox("Select Target Column", df.columns)
+        feature_column = st.sidebar.selectbox("Select Feature to Compare with Target", df.columns)
+        if df[feature_column].dtype == 'object':
+            if (df[target_column].dtype == 'object'): 
+                fig = px.histogram(df, x=feature_column, color=target_column, 
+                                    title=f"Comparison of {feature_column} with {target_column}")
                 st.plotly_chart(fig)
             else:  
-                fig = px.box(df, x=col, y=target_column, 
-                             title=f"Box Plot of {target_column} by {col}",color_discrete_sequence=["#8A2BE2"])
+                fig = px.box(df, x=feature_column, y=target_column, 
+                             title=f"Box Plot of {target_column} by {feature_column}",color_discrete_sequence=["#8A2BE2"])
+                st.plotly_chart(fig)
+
+        elif df[feature_column].dtype in ['int64', 'float64']:
+            if (df[target_column].dtype == 'object'): 
+                fig = px.box(df, x=feature_column, y=target_column, 
+                             title=f"Box Plot of {target_column} by {feature_column}",color_discrete_sequence=["#8A2BE2"])
+                st.plotly_chart(fig)
+            else:  
+                fig = px.scatter(df, x=feature_column, y=target_column, 
+                             title=f"Comparison of {feature_column} with {target_column}",color_discrete_sequence=["#FF1493"])
                 st.plotly_chart(fig)
 
 
 def main():
-    st.title("CSV Data Analysis App")
-    uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
+    st.title("Data Preprocessing and Model Building App")
     
-    if uploaded_file is not None:
-        df = load_data(uploaded_file)
-        st.write("Dataframe Loaded:")
-        st.dataframe(df.head())
-        st.subheader("Select Columns to Delete")
-        columns_to_delete = st.multiselect("Select columns to delete", df.columns.tolist())
-        
-        if columns_to_delete:
-            df = df.drop(columns=columns_to_delete)
-            st.write(f"Updated Dataframe after deleting columns {columns_to_delete}:")
-            st.dataframe(df.head())
-        
-        null_values, duplicate_values = data_checks(df)
-        st.write("Null Values in Each Column:")
-        st.write(null_values)
-        st.write(f"Number of Duplicate Rows: {duplicate_values}")
-        st.write(f'Shape of Dataframe: {df.shape}')
-        
-        visualize_numerical(df)
-        visualize_categorical(df)
-        
-        target_column = st.selectbox("Select Target Column", df.columns.tolist())
-        st.write(f"Target Column: {target_column}")
-        
-        compare_checkbox = st.checkbox("Compare Columns with Target Column")
-        
-        if compare_checkbox:
-            compare_with_target(df, target_column)
-
-
+    df = load_csv()
+    
+    if df is not None:
+        df = preprocess_data(df)
+        visualize_data(df)
+    
 if __name__ == "__main__":
     main()
+
